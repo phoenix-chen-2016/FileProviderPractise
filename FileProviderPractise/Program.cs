7786e4d1321c78157761ad8d97ff645bbbd269db
Phoenix <fphoenix923@hotmail.com>
2024-11-03 14:10:40 +0800

    加入專案檔案。

---
+++ FileProviderPractise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.FileProviders;
+
+var fileProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+
+using var fs = fileProvider.GetFileInfo("/Assets/data.txt")
+	.CreateReadStream();
+using var reader = new StreamReader(fs);
+
+var text = reader.ReadToEnd();
+
+Console.WriteLine(text);
+
+Console.ReadKey(false);
